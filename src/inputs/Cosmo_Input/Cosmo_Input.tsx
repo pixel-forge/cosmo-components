be0823440fcf3319c14e56c2564c21f197782919
Cipher <itayleybovich@gmail.com>
2023-11-20 00:42:15 +0200

    Cosmo_Input placeholder and design

--- src/inputs/Cosmo_Input/Cosmo_Input.tsx
+++ src/inputs/Cosmo_Input/Cosmo_Input.tsx
@@ -1,27 +1,29 @@
 import * as React from 'react';
 import {Cosmo_SyncComponent} from '../../base/Cosmo_SyncComponent';
-import { _className } from '@pixel-forge/cosmo-utils';
+import {_className} from '@pixel-forge/cosmo-utils';
+import './Cosmo_Input.scss';
 
 type Props_Base = {
 	style?: React.CSSProperties;
 	className?: string;
-	id?:string;
-	disabled?:boolean;
+	id?: string;
+	disabled?: boolean;
+	placeholder?: string;
 };
 
-type Props_NumberInput = Props_Base & {
+type Props_NumberInput = {
 	type: 'number';
-	value: number | undefined;
-	onChange: (value: string) => void;
+	value?: number;
+	onChange: (value?: number) => void;
 }
 
-type Props_TextInput = Props_Base & {
+type Props_TextInput = {
 	type: 'text';
-	value: string | undefined;
-	onChange: (value: string) => void;
+	value?: string;
+	onChange: (value?: string) => void;
 }
 
-type Props = Props_NumberInput | Props_TextInput;
+type Props = Props_Base & (Props_TextInput | Props_NumberInput);
 
 type State = {
 	value?: string | number;
@@ -41,20 +43,26 @@ export class Cosmo_Input
 
 	private onValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const value = e.target.value;
-		this.props.onChange(value);
-	}
+		const resolvedValue = this.props.type === 'text'
+			? (value === '' ? undefined : value)
+			: (value === '' ? undefined : Number(value));
+
+		//@ts-ignore
+		this.props.onChange(resolvedValue);
+	};
 
 	// ################## Class Render ##################
 
-	render () {
-		const className = _className('cosmo-input',this.props.className);
+	render() {
+		const className = _className('cosmo-input', this.props.className);
 		return <input
 			id={this.props.id}
 			type={this.props.type}
-			value={this.props.value}
+			value={this.props.value ?? ''}
 			onChange={this.onValueChange}
 			className={className}
 			disabled={this.props.disabled}
-		/>
+			placeholder={this.props.placeholder}
+		/>;
 	}
 }
